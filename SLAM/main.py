d31679ad2ccfe2906b70ab1bc61da485e793725a
shmupISCE <srdjan.mprivate@gmail.com>
2021-07-18 21:44:28 +0200

    Added matchTemplate()

--- SLAM/main.py
+++ SLAM/main.py
@@ -3,6 +3,4 @@ import cv2 as cv
 ores = cv.imread('ores.jpg',cv.IMREAD_UNCHANGED)
 lapis = cv.imread('lapis_ore.jpg', cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate()
-
-def fu
+result = cv.matchTemplate(ores, lapis, cv.TM_CCOEFF_NORMED)
